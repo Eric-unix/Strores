54c0e3390dbbd6907b5147ba93d78c662acc744e
Erix-Unix <erixlopez6@gmail.com>
2021-12-08 19:34:11 -0600

    Fragment

---
+++ app/src/main/java/com/example/stores/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.stores
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.stores.databinding.ActivityMainBinding
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+    private lateinit var mBinding: ActivityMainBinding
+
+    private lateinit var mAdapter: StoreAdapter
+    private lateinit var mGridLayout: GridLayoutManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mBinding.btnSave.setOnClickListener {
+            val store = StoreEntity(name = mBinding.etName.text.toString().trim())
+
+            Thread {
+                StoreApplication.database.storeDao().addStore(store)
+            }.start()
+
+            mAdapter.add(store)
+        }
+
+        setupRecylcerView()
+    }
+
+    private fun setupRecylcerView() {
+        mAdapter = StoreAdapter(mutableListOf(), this)
+        mGridLayout = GridLayoutManager(this, 2)
+        getStores()
+
+        mBinding.recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = mGridLayout
+            adapter = mAdapter
+        }
+    }
+
+    private fun getStores(){
+        doAsync {
+            val stores = StoreApplication.database.storeDao().getAllStores()
+            uiThread {
+                mAdapter.setStores(stores)
+            }
+        }
+    }
+
+    /*
+    * OnClickListener
+     */
+    override fun onClick(storeEntity: StoreEntity) {
+    }
+
+    override fun onFavoriteStore(storeEntity: StoreEntity) {
+        storeEntity.isFavorite = !storeEntity.isFavorite
+        doAsync {
+            StoreApplication.database.storeDao().updateStore(storeEntity)
+            uiThread {
+                mAdapter.update(storeEntity)
+            }
+        }
+    }
+
+    override fun onDeleteStore(storeEntity: StoreEntity) {
+        doAsync {
+            StoreApplication.database.storeDao().deleteStore(storeEntity)
+            uiThread {
+                mAdapter.delete(storeEntity)
+            }
+
+        }
+    }
+}
